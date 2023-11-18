9019bcbc6ff19cd23548c1633bd7558e12644b92
johnnycupcakes <nicboutte@gmail.com>
2023-11-18 01:31:25 -0800

    Remove component/navbar folder

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,8 +1,6 @@
 import './globals.css'
 import { Lora } from 'next/font/google'
-
-// components
-import Navbar from './components/Navbar'
+import Link from 'next/link'
 
 const lora = Lora({ subsets: ['latin'] })
 
@@ -15,7 +13,18 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={lora.className}>
-      <Navbar />
+      <nav>
+        <h1>Take Home Assessment</h1>
+        <Link href="/">Home </Link>
+        <Link href="/1">Question 1</Link>
+        <Link href="/2">Question 2</Link>
+        <Link href="/3">Question 3</Link>
+        <Link href="/4">Question 4</Link>
+        <Link href="/5">Question 5</Link>
+        <h2>What response would you write back?</h2>
+        <Link href="/wrwywb/1">Question 1</Link>
+        <Link href="/wrwywb/2">Question 2</Link>
+      </nav>
       {children}</body>
     </html>
   )
